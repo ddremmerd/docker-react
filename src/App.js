497fa01717db40040412b644a55c1b69ff65299b
suwanan.k <suwanannn.k@gmail.com>
2022-01-09 18:49:25 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          HEEEEELLLLLLLOOOOOOOOOOOOO
+
+          fsgdfsgsdfgdfhgsfd44535
+
+          CHANGEEEEDDD HERE BITCH!!!
+
+          hakrrrrrooooo ALOHA GU DID ITs
+        
+        WINTER AESPA
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
